d9bee6589a8051630537e325da20c3f66757dba5
Drakedragoo <163123866+Drakedragoo@users.noreply.github.com>
2024-09-24 08:38:42 -0300

    Delete exercicio 3 directory

--- exercicio 3/atividade3(py)/inversa.py
+++
@@ -1,8 +0,0 @@
-vetor = [0] * 5
-
-for i in range(5):
-    vetor[i] = int(input(f"Digite o elemento {i + 1}: "))
-
-print("Elementos do vetor na ordem inversa:")
-for elemento in vetor[::-1]:
-    print(elemento)
